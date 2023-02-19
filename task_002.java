abebced14623e152f669789ea1118df8f00cadbe
Zhestkow <113130350+Zhestkow@users.noreply.github.com>
2023-02-19 19:49:25 +0300

    HW_02

--- task_002.java
+++ task_002.java
@@ -1,26 +1,59 @@
-import java.util.Scanner;
+/**
+ * Реализуйте алгоритм сортировки пузырьком числового массива, результат после каждой итерации запишите в лог-файл.
+ */
 
-// Вывести все простые числа от 1 до 1000
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class task_002 {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Введите число ");
-        int num = scan.nextInt();
-        scan.close();
-        for (int i = 2; i <= num; i++) {
-            Boolean flag = true;
-            for (int j = 2; j < i / 2; j++) {
-                if (i % j == 0) {
-                    flag = false;
-                    break;
+
+    public static int[] randomArr() {
+        Random rand = new Random();
+        int arr[] = new int[10];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rand.nextInt(100);
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println("");
+        return arr;
+    }
+
+    public static int[] babblSort(int arr[]) throws IOException {
+        Logger loger = Logger.getLogger(task_002.class.getName());
+        FileHandler fHandler = new FileHandler("Task2_2.txt");
+        SimpleFormatter sFormatter = new SimpleFormatter();
+        fHandler.setFormatter(sFormatter);
+        loger.addHandler(fHandler);
+
+        int temp;
+        for (int i = arr.length - 1; i >= 0; i--) {
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (arr[j] < arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
-            if (flag) {
-                System.out.println(i);
-            }
 
+            loger.info(Arrays.toString(arr));
         }
+        return arr;
+
+    }
+
+    public static void print(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        print(babblSort(randomArr()));
     }
 
 }
--- task_003.java
+++ task_003.java
@@ -1,47 +1,79 @@
-import java.util.Scanner;
+/**
+ * В файле содержится строка с данными:
+[{"фамилия":"Иванов","оценка":"5","предмет":"Математика"}, 
+{"фамилия":"Петрова","оценка":"4","предмет":"Информатика"}, 
+{"фамилия":"Краснов","оценка":"5","предмет":"Физика"}]
+Напишите метод, который разберёт её на составные части и, используя StringBuilder создаст массив строк такого вида:
+Студент Иванов получил 5 по предмету Математика.
+Студент Петрова получил 4 по предмету Информатика.
+Студент Краснов получил 5 по предмету Физика.
+ */
 
-// Реализовать простой калькулятор Введите число 2 
-// Введите операцию 1 + 2 - 3 * 4 / Введите число 2 2 + 2 = 4
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class task_003 {
+    public static String getLineOffile(String fileName) {
+        String strOffile = null;
+        try {
+            File file = new File(fileName);
+            FileReader fr = new FileReader(file);
+            BufferedReader reader = new BufferedReader(fr);
+            strOffile = reader.readLine();
 
-    static int getResult(String opr, int num1, int num2) {
-        int result = 0;
-        switch (opr) {
-            case "+":
-                result = num1 + num2;
-                break;
-            case "-":
-                result = num1 - num2;
-                break;
-            case "*":
-                result = num1 * num2;
-                break;
-            case "/":
-                result = num1 / num2;
-
-            default:
-                System.out.println("Введите оператор: -, +, *, /");
-                break;
+            reader.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        return result;
 
+        return strOffile;
+    }
+
+    private static String[][] jsnparsetoarray(String jsnstr) {
+        String line = jsnstr.substring(1, jsnstr.length() - 1);
+
+        String[] jsnarray = line.split(", ");
+
+        String[][] jsnarrayofBase = new String[jsnarray.length][3];
+        for (int i = 0; i < jsnarray.length; i++) {
+            line = jsnarray[i].substring(1, jsnarray[i].length() - 1);
+            for (int j = 0; j < 3; j++) {
+                String[] minJsnarray = line.split(",");
+                String[] microJsnarray = minJsnarray[j].split(":");
+                jsnarrayofBase[i][j] = microJsnarray[1].substring(1, microJsnarray[1].length() - 1);
+
+            }
+        }
+        return jsnarrayofBase;
+    }
+
+    private static String[] gluing_strings(String[][] arraystr) {
+        String[] gluing_str = new String[arraystr.length];
+        StringBuilder Newstr = new StringBuilder();
+        for (int i = 0; i < arraystr.length; i++) {
+            Newstr.append("Студент ");
+            Newstr.append(arraystr[i][0]);
+            Newstr.append(" получил  ");
+            Newstr.append(arraystr[i][1]);
+            Newstr.append(" по предмету ");
+            Newstr.append(arraystr[i][2]);
+            gluing_str[i] = Newstr.toString();
+            Newstr.delete(0, Newstr.length());
+        }
+        return gluing_str;
     }
 
     public static void main(String[] args) {
-        System.out.println("Введите первое число: ");
-        Scanner scan = new Scanner(System.in);
-        int num1 = scan.nextInt();
-        System.out.println("Введите оператор: -, +, *, /");
-        Scanner scanOp = new Scanner(System.in);
-        String operations = scanOp.nextLine();
-        System.out.println("Введите второе число: ");
-        int num2 = scan.nextInt();
-        int result = getResult(operations, num1, num2);
-        System.out.printf("%d %s %d = %d", num1, operations, num2, result);
-        scan.close();
-        scanOp.close();
-
+        String strjsn_offile = getLineOffile("text2.jsn");
+        System.out.println(strjsn_offile);
+        String[] array = gluing_strings(jsnparsetoarray(strjsn_offile));
+        for (int i = 0; i < array.length; i++)
+            System.out.println(array[i]);
     }
-
 }
